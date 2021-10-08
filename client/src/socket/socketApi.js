f9152439b287369ce5697dc423732485dcc8760a
Oğuz Ölke <oguzolke@gmail.com>
2021-10-08 17:17:36 +0300

    server deployed to heroku

--- client/src/socket/socketApi.js
+++ client/src/socket/socketApi.js
@@ -3,8 +3,9 @@ import { io } from 'socket.io-client';
 let socket;
 export const connectSocket = () => {
   console.log('Connectting');
-  socket = io('http://localhost:8000/', { transports: ['websocket'] });
-  // socket = io(process.env.REACT_APP_BACKEND_ENDPOINT, { transports: ['websocket'] });
+  socket = io('https://intense-crag-03696.herokuapp.com/', {
+    transports: ['websocket'],
+  });
 
   socket.on('connect', () => {
     console.log('connected');
